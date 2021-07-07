4cb628ea43554b41b104ed74de3be8e727ac5668
Jason <oblitorum@gmail.com>
2021-07-08 03:51:04 +0800

    fix: link to homepage

--- src/common/navbar.tsx
+++ src/common/navbar.tsx
@@ -1,16 +1,19 @@
 import { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default class Navbar extends Component {
   render() {
     return (
       <nav className="has-text-white has-background-primary" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-          <a style={{fontSize: "1.5rem"}} className="navbar-item has-text-white" href="/">
-            <figure className="image is-24x24 mr-3">
-              <img src={require('common/logo.svg').default} />
-            </figure>
-            <span>DevApps</span>
-          </a>
+          <NavLink to="/">
+            <div style={{fontSize: "1.5rem"}} className="navbar-item has-text-white">
+              <figure className="image is-24x24 mr-3">
+                <img src={require('common/logo.svg').default} />
+              </figure>
+              <span>DevApps</span>
+            </div>
+          </NavLink>
         </div>
       </nav>
     )
